f3db13b7becf79c56264eacf8f6894f9ff212a31
Jack Watters <squawfreeskier@gmail.com>
2019-05-17 10:55:16 -0700

    index

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
 const ContactForm = require('./routes/contactForm');
 
 const app = express();
@@ -10,11 +9,11 @@ app.use(bodyParser.json());
 //could also be        var cb = bodyParser.json();
 //                     app.use(cb);
 
-
+app.get('/', function (req, res) {
+    res.send('Hello World!')
+  })
 //app.use will only match with request that start with api/v1/form/ sends request to contactform
 
 app.use('/api/v1/form', ContactForm);
 
-const PORT = process.env.PORT || 4020;
-app.listen(PORT);
-console.log(`Listening on port ${PORT}`);
+module.exports = app;
